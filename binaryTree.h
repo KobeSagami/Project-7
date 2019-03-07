389c1ed4dc71900b5221289d5aff0ecd3a84a120
Branden <branden.ramsdell@icloud.com>
2019-03-07 15:21:32 -0700

    added stub code

--- binaryTree.h
+++ binaryTree.h
@@ -1,1 +1,25 @@
+/* Project 7 - Binary Search Tree
+** 16 March 2019
+** Branden Ramsdell
+** Kobe Sagami
+** Hannah Hollenback */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "binaryNode.h"
+
+typedef struct Binary_Tree_Struct{
+    binaryNode root;
+    
+} binary_tree;
+
+void initialize(binary_tree* bt);
+bool search(binary_tree* bt, int key);
+void insert(binary_tree* bt, int item);
+void printinorder(binary_tree* bt);
+void printpreorder(binary_tree* bt);
+void printpostorder(binary_tree* bt);
+int btsize(binary_tree* bt);
+int treeheight(binary_tree* bt);
 
