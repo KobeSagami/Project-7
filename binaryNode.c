6c734e7e43101d62ad5e74cae70967e6a3a7af8e
Branden Ramsdell <branden@Brandens-iMac.local>
2019-03-14 08:45:31 -0600

    Added HasLeaves functionality to binaryNode.c

--- binaryNode.c
+++ binaryNode.c
@@ -1,1 +1,11 @@
 #include "binaryNode.h"
+
+bool HasLeaves(BinaryNode* bn){
+     //If either leaf exists, returns true, else false.
+     if(bn->leftNode != NULL || bn->rightNode != NULL){
+          return true;
+     }
+     else{
+          return false;
+     }
+}
